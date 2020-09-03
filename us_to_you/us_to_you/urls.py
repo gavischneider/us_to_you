e0dd319ed5f4515a3f2db493d54be63b69e43096
Gavi Schneider <gavischneider@gmail.com>
2020-09-03 05:36:18 +0300

    initial commit

---
+++ us_to_you/us_to_you/urls.py
@@ -0,0 +1,39 @@
+"""us_to_you URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+from django.conf.urls import url, include
+from rest_framework.authtoken import views as drf_views
+from django.urls import path
+from . import views
+from django.urls import path
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html'))
+]
+
+"""
+path("login", views.login_view, name="login"),
+path("logout", views.logout_view, name="logout"),
+path("register", views.register, name="register"),
+path("about_us", views.about_us, name="about_us")
+
+"""
